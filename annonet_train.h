c2db2367887b57c07bccb571e692b6aae5f742ae
Juha Reunanen <juha.reunanen@tomaattinen.com>
2020-04-24 13:29:47 +0300

    Apparently we need stronger assertions (for now at least)

--- annonet_train.h
+++ annonet_train.h
@@ -90,21 +90,21 @@ dlib::rectangle random_rect_containing_point(
     const dlib::rectangle& limits
 )
 {
-    DLIB_ASSERT(limits.contains(point));
-    DLIB_ASSERT(result_width <= limits.width());
-    DLIB_ASSERT(result_height <= limits.height());
+    DLIB_CASSERT(limits.contains(point));
+    DLIB_CASSERT(result_width <= limits.width());
+    DLIB_CASSERT(result_height <= limits.height());
     const long min_center_x = std::max(limits.left() + result_width / 2, point.x() - (result_width - 1) / 2);
     const long max_center_x = std::min(limits.right() - (result_width - 1) / 2, point.x() + result_width / 2);
     const long min_center_y = std::max(limits.top() + result_height / 2, point.y() - (result_height - 1) / 2);
     const long max_center_y = std::min(limits.bottom() - (result_height - 1) / 2, point.y() + result_height / 2);
-    DLIB_ASSERT(max_center_x >= min_center_x);
-    DLIB_ASSERT(max_center_y >= min_center_y);
+    DLIB_CASSERT(max_center_x >= min_center_x);
+    DLIB_CASSERT(max_center_y >= min_center_y);
     const long center_x = min_center_x + rnd.get_random_32bit_number() % (max_center_x - min_center_x + 1);
     const long center_y = min_center_y + rnd.get_random_32bit_number() % (max_center_y - min_center_y + 1);
     const auto rect = dlib::centered_rect(dlib::point(center_x, center_y), result_width, result_height);
-    DLIB_ASSERT(rect.width() == result_width);
-    DLIB_ASSERT(rect.height() == result_height);
-    DLIB_ASSERT(limits.contains(rect));
-    DLIB_ASSERT(rect.contains(point));
+    DLIB_CASSERT(rect.width() == result_width);
+    DLIB_CASSERT(rect.height() == result_height);
+    DLIB_CASSERT(limits.contains(rect));
+    DLIB_CASSERT(rect.contains(point));
     return rect;
 };
